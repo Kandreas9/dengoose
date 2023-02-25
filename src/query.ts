b440ddefdce7d171dd955f5b0765305be5ea8c0e
Kandreas9 <andkarido@gmail.com>
2023-02-25 22:41:28 +0100

    connected to docker mongo db and created user

--- src/query.ts
+++ src/query.ts
@@ -2,10 +2,23 @@ import { MongoClient } from "../deps.ts";
 
 const client = new MongoClient();
 
-await client.connect({
-  db: "dengoose",
-  credential: {
-    username: "dengoose",
-    password: "password",
-  },
+await client.connect('mongodb://127.0.0.1:27017/test');
+
+// Defining schema interface
+interface UserSchema {
+  _id: ObjectId;
+  username: string;
+  password: string;
+}
+
+const db = client.database("test");
+const users = db.collection<UserSchema>("users");
+
+const insertId = await users.insertOne({
+  username: "user1",
+  password: "pass1",
 });
+
+const user1 = await users.findOne({ _id: insertId });
+
+console.log(user1);
